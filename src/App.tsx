01f7060994d9ced64b08782c80df7af1f54b0e06
pedroapinheiro <pedro.azevedop18@hotmail.com>
2023-04-13 13:20:45 -0300

    Adicionando página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
-
+    </BrowserRouter>
   );
 }
 
 export default App;
-
